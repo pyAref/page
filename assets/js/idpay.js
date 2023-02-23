830ce4b0f5015d84d447384b5c5ce90a4358e1ce
pyaref <arefty56gh@gmail.com>
2023-02-23 14:13:18 +0330

    change script file location

---
+++ assets/js/idpay.js
@@ -0,0 +1,5 @@
+const success_confirm_payment = document.getElementById("success_confirm_payment");
+function ChangeMessage() {
+    document.title = 'درحال انتقال ...';
+    success_confirm_payment.innerHTML = '...در حال انتقال به سایت پذیرنده';
+}
